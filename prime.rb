8f5f5a7da406e5bf53aba7cfdf5abf896a2fd13c
Joseph Romero <joeromero47@gmail.com>
2020-04-23 18:15:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,4 +3,6 @@ def prime?(number)
   if number > 1
       range = (start..number-1).to_a
       range.none? do |num_to_test|
-        number % num_to_test
+        number % num_to_test == 0
+    end
+  end
